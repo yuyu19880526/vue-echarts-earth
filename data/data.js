0a5342f91c1d5a5b8414f4c670128da1d7338138
liujingmin <liujingmin@360.cn>
2022-01-07 10:34:49 +0800

    fix

---
+++ data/data.js
@@ -0,0 +1,42 @@
+var json = [
+	// {
+    //     victim_ip: "430.254.3.192",
+    //     attacked_point: [116.40, 39.90],
+    //     attack_date: '2019-06-10 18:40:43',
+    //     source_ip: '192.168.254.222', 
+    //     source_point: [123.43, 41.80],
+    //     alert_level: '严重',           
+    //     attacked_city: '北京',              
+    //     source_city: '辽宁',                
+    //     attack_count: 120,			
+    //     attacked_domain: 'baidu.com',		
+    //     attack_mode: 'SQL注入'	
+    // }
+    {
+        victim_ip: "43.254.3.192",
+        attacked_point: [113.92, 22.52],    
+        attack_name: '发现病毒文件', 
+        source_ip: '192.168.254.208',     
+        source_point: [123.43, 41.80],
+        alert_level: '高危',           
+        attacked_city: '深圳',           
+        source_city: '辽宁',              
+        attack_count: 250,					
+        attacked_type: '勒索病毒',		
+        attack_mode: 'web登录尝试',
+        country: '中国'
+    },
+    // {
+    //     victim_ip: "43.254.3.192",
+    //     attacked_point: [84.87, 45.60],    
+    //     attack_date: '2019-02-18 18:40:43', 
+    //     source_ip: '192.168.254.208',     
+    //     source_point: [123.43, 41.80],
+    //     alert_level: '中危',           
+    //     attacked_city: '克拉玛依',           
+    //     source_city: '辽宁',              
+    //     attack_count: 250,                  
+    //     attacked_domain: 'souhu.com',       
+    //     attack_mode: 'web登录尝试'
+    // }
+]
